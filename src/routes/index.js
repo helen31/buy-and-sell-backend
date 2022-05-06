a8fa4154c24ee3d5adc0aaf74aa96d135ebf5582
Olena_Hlukhovska <a30058422@gmail.com>
2022-05-06 15:59:09 +0200

    add getUserListingsRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { getAllListingsRoute } from './get-all-listings';
 import { getListingRoute } from './get-listing';
 import { addViewToListingRoute } from './add-view-to-listing';
+import { getUserListingsRoute } from './get-user-listings';
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
+    getUserListingsRoute,
 ];
