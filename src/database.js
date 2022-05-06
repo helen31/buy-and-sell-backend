23207ba764d5fb725d1d365bba5f183fa04681fb
Olena_Hlukhovska <a30058422@gmail.com>
2022-05-06 12:56:16 +0200

    * install mysql * setup mysql db  in server.js * redone endpoints

---
+++ src/database.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hapi-server',
+    password: 'abc123',
+    database: 'buy-and-sell',
+});
+
+export const db = {
+    connect: () => connection.connect(),
+    query: (queryString, escapedValues) => {
+        return new Promise((res, rej) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if(error) {
+                    rej(error);
+                }
+                res({results, fields});
+            })
+        });
+    },
+    end: () => connection.end(),
+}
