a7675a7051a8e7d80c249d8824621bbf7fb740d9
Olena_Hlukhovska <a30058422@gmail.com>
2022-05-06 13:11:01 +0200

    add addViewToListingRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { getAllListingsRoute } from './get-all-listings';
-import { getListingRoute } from "./get-listing";
+import { getListingRoute } from './get-listing';
+import { addViewToListingRoute } from './add-view-to-listing';
 
 export default [
     getAllListingsRoute,
     getListingRoute,
+    addViewToListingRoute,
 ];
