7f422371ba1af5eff3848899dd32884b76a1dd47
Olena_Hlukhovska <a30058422@gmail.com>
2022-05-06 16:17:54 +0200

    * install uuid to generate ids * add createNewListingRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import { getAllListingsRoute } from './get-all-listings';
 import { getListingRoute } from './get-listing';
 import { addViewToListingRoute } from './add-view-to-listing';
 import { getUserListingsRoute } from './get-user-listings';
+import { createNewListingRoute } from './create-new-listing';
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
     getUserListingsRoute,
+    createNewListingRoute,
 ];
